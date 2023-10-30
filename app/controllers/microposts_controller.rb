282e3889f7cdef68ad9576c6b3519f71e4be7e45
EC2 Default User <124329029+shouichi161@users.noreply.github.com>
2023-10-30 15:15:42 +0000

    [add]micropostの新規投稿を作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,12 @@
 class MicropostsController < ApplicationController
   def new
+    @micropost = Micropost.new
+  end
+  def create
+    micropost = Micropost.new(micropost_params)
+    micropost.user_id = current_user.id
+    micropost.save
+    redirect_to microposts_path
   end
 
   def index
@@ -9,5 +16,11 @@ class MicropostsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit[:content]
   end
 end
